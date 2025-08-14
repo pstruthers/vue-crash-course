dbf9ba349a6b91b980b702990149f8b0ffbc59c8
Paige Struthers <pstruthers@unomaha.edu>
2025-08-14 16:59:08 -0500

    Composition API: long version

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <script>
+import { ref } from 'vue'
+
 export default {
-  data() {
-    return {
-      name: 'Paige Struthers',
-      status: 'pending',
-      tasks: ['Task One', 'Task Two', 'Task Three'],
-      link: 'https://www.google.com',
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending'
-      } else if (this.status === 'pending') {
-        this.status = 'inactive'
+  setup() {
+    const name = ref('Paige Struthers')
+    const status = ref('active')
+    const tasks = ref(['Task One', 'Task Two', 'Task Three'])
+
+    const toggleStatus = () => {
+      if (status.value === 'active') {
+        status.value = 'pending'
+      } else if (status.value === 'pending') {
+        status.value = 'inactive'
       } else {
-        this.status = 'active'
+        status.value = 'active'
       }
-    },
+    }
+
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    }
   },
 }
 </script>
@@ -31,8 +36,6 @@ export default {
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <!-- <a v-bind:href="link">Click for Google</a> -->
-  <a :href="link">Click for Google</a>
   <br />
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button @click="toggleStatus">Change Status</button>

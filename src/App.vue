d0b4c0e793ae4dec7d330bd27c29cc8d52136539
Paige Struthers <pstruthers@unomaha.edu>
2025-08-14 16:48:06 -0500

    Options API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Paige Struthers',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://www.google.com',
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    },
+  },
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
 </template>
-
-<style scoped></style>
